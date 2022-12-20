d848befe81f106db46cc7ae2c96fb22b409cbebb
Firman Ramadhan <firman19ramadhan@gmail.com>
2022-12-20 15:21:17 +0700

    new update

---
+++ src/components/LoginInput.test.js
@@ -0,0 +1,28 @@
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle username typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import LoginInput from './LoginInput';
+
+describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => {}} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+
+    // Action
+    await userEvent.type(usernameInput, 'usernametest');
+
+    // Assert
+    expect(usernameInput).toHaveValue('usernametest');
+  });
+});
